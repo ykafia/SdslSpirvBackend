775c23610640c43aea318eceb3415798b196d448
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2022-03-29 16:24:19 +0200

    Modified Sdsl files

--- TestStrideSpirv/Program.cs
+++ TestStrideSpirv/Program.cs
@@ -6,6 +6,8 @@ using Stride.Core.Shaders.Parser;
 using Stride.Core.Shaders.Grammar.Stride;
 using SPIRVCross;
 using static SPIRVCross.SPIRV;
+using System.Linq;
+using Stride.Core.Shaders.Ast.Stride;
 
 Console.WriteLine("Hello, World!");
 
@@ -13,11 +15,13 @@ var grammar = ShaderParser.GetGrammar<StrideGrammar>();
 
 var parser = ShaderParser.GetParser<StrideGrammar>();
 
-// var shaderText = File.ReadAllText("./Test.sdsl");
+var sdslPath = "./Shaders/SDSL/TestVertexStreamFull.sdsl";
+var shaderText = File.ReadAllText(sdslPath);
 
-// var shader = parser.Parse(shaderText, "./Test.sdsl").Shader;
-// Console.WriteLine(shader);
+var shader = parser.Parse(shaderText, sdslPath).Shader;
+Console.WriteLine(shader.Declarations.Where(x => x is ShaderClassType).Count());
+Console.WriteLine(shader);
 
-var vertPath = @"C:\Users\youness_kafia\Documents\GitHub\SdslSpirvBackend\TestStrideSpirv\Shaders\GLSL\tri.vert.spv";
+// var vertPath = @"C:\Users\youness_kafia\Documents\GitHub\SdslSpirvBackend\TestStrideSpirv\Shaders\GLSL\tri.vert.spv";
 
-System.IO.File.ReadAllBytes(vertPath).ToGlsl();
+// System.IO.File.ReadAllBytes(vertPath).ToGlsl();
