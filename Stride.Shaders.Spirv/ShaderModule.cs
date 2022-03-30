77e2df7588816d70bf918ffa8178e6ce996801d7
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2022-03-30 18:17:11 +0200

    Register types

--- Stride.Shaders.Spirv/ShaderModule.cs
+++ Stride.Shaders.Spirv/ShaderModule.cs
@@ -12,6 +12,11 @@ namespace Stride.Shaders.Spirv
     {
         Shader program;
         Dictionary<string,Instruction> TypeRegister = new();
+        Dictionary<string, Instruction> InputRegister = new();
+        Dictionary<string, Instruction> OutputRegister = new();
+        Dictionary<string, Instruction> StreamsRegister = new();
+        
+        
 
         public ShaderModule(Shader program) : base(Specification.Version)
         {
@@ -24,7 +29,12 @@ namespace Stride.Shaders.Spirv
             SetMemoryModel(AddressingModel.Logical, MemoryModel.Simple);
             foreach(StructType p in program.Declarations.Where(x => x is StructType))
             {
-                Console.WriteLine(p);
+                if(p.Name.Text.Contains("INPUT"))
+                    GenerateInputs(p);
+                if(p.Name.Text.Contains("OUTPUT"))
+                    GenerateOutputs(p);
+                if(p.Name.Text.Contains("STREAMS"))
+                    GenerateStream(p);
             }
 
             // TODO: add void function
@@ -34,23 +44,59 @@ namespace Stride.Shaders.Spirv
             s.Fields.ForEach(
                 f =>
                 {
-                    f.Type.Name
+                    InputRegister.Add(f.Name,TypePointer(StorageClass.Input,GetOrCreateSPVType(f.Type.Name)));
                 }
             );
         }
-        public Instruction GetOrCreateSPVType(TypeBase t)
+        public void GenerateOutputs(StructType s)
         {
-            if(TypeRegister.ContainsKey(t.Name))
+            s.Fields.ForEach(
+                f =>
+                {
+                    OutputRegister.Add(f.Name,TypePointer(StorageClass.Output,GetOrCreateSPVType(f.Type.Name)));
+                }
+            );
+        }
+        public void GenerateStream(StructType s)
+        {
+            s.Fields.ForEach(
+                f =>
+                {
+                    StreamsRegister.Add(f.Name,TypePointer(StorageClass.Output,GetOrCreateSPVType(f.Type.Name)));
+                }
+            );
+        }
+        public Instruction GetOrCreateSPVType(string t)
+        {
+            if(TypeRegister.ContainsKey(t))
             {
-                return TypeRegister[t.Name];
+                return TypeRegister[t];
             }
             // TODO : Create type instruction SPV
-            return null;
+            TypeRegister[t] = SDSLTypeToSPVType(t);
+            return TypeRegister[t];
         }
 
-        private static void SDSLTypeToSPVType()
+        private Instruction SDSLTypeToSPVType(string name)
         {
-
+            return name switch 
+            {
+                "half" => TypeFloat(16),
+                "float" => TypeFloat(32),
+                "double" => TypeFloat(64),
+                "ushort" => TypeInt(16,0),
+                "uint" => TypeInt(32,0),
+                "ulong" => TypeInt(64,0),
+                "short" => TypeInt(16,1),
+                "int" => TypeInt(32,1),
+                "long" => TypeInt(64,1),
+                "byte" => TypeInt(0,1),
+                "sbyte" => TypeInt(8,1),
+                "float2" => TypeVector(GetOrCreateSPVType("float"),2),
+                "float3" => TypeVector(GetOrCreateSPVType("float"),3),
+                "float4" => TypeVector(GetOrCreateSPVType("float"),4),
+                _ => throw new Exception("Type not found")
+            };
         }
     }
 }
